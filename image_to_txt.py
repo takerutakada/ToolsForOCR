41ffda577009e098d72b1d222a3a1b81002e413e
takerutakada <mnmnmmmn@outlook.jp>
2022-03-09 22:37:44 +0900

    image_to_txtモジュール化

--- image_to_txt.py
+++ image_to_txt.py
@@ -1,36 +1,40 @@
 from PIL import Image
-import sys
+from sys import exit
+from pyperclip import copy
+from pyocr import tesseract, get_available_tools
+from pyocr.builders import TextBuilder
 
-import pyperclip
+def image_to_txt(lang):
+    # Windows
+    tesseract.TESSERACT_CMD = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-import pyocr
-import pyocr.builders
+    tools = get_available_tools()
+    if len(tools) == 0:
+        print("No OCR tool found")
+        exit(1)
+    # The tools are returned in the recommended order of usage
+    tool = tools[0]
+    print("Will use tool '%s'" % (tool.get_name()))
+    # Ex: Will use tool 'libtesseract'
 
-tools = pyocr.get_available_tools()
-if len(tools) == 0:
- print("No OCR tool found")
- sys.exit(1)
-# The tools are returned in the recommended order of usage
-tool = tools[0]
-print("Will use tool '%s'" % (tool.get_name()))
-# Ex: Will use tool 'libtesseract'
+    langs = tool.get_available_languages()
+    print("Available languages: %s" % ", ".join(langs))
+    print("Will use lang '%s'" % (lang))
+    # Ex: Will use lang 'fra'
+    # Note that languages are NOT sorted in any way. Please refer
+    # to the system locale settings for the default language
+    # to use.
 
-langs = tool.get_available_languages()
-print("Available languages: %s" % ", ".join(langs))
-lang = langs[0]
-print("Will use lang '%s'" % (lang))
-# Ex: Will use lang 'fra'
-# Note that languages are NOT sorted in any way. Please refer
-# to the system locale settings for the default language
-# to use.
+    txt = tool.image_to_string(
+        Image.open('test.png'),
+        lang=lang,
+        builder=TextBuilder()
+    )
+    # txt is a Python string
 
-txt = tool.image_to_string(
- Image.open('test.png'),
- lang=lang,
- builder=pyocr.builders.TextBuilder()
-)
-# txt is a Python string
+    print(txt)
+    copy(txt)
+    print('copied to clipboard.')
 
-print(txt)
-pyperclip.copy(txt)
-print('copied to clipboard.')
+if __name__ == '__main__':
+    image_to_txt('jpn')
