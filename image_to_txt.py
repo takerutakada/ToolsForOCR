84b52ecdaf6e487bc1d9dd74e44e1a5b00d61fc7
takerutakada <mnmnmmmn@outlook.jp>
2022-03-10 17:11:55 +0900

    image_to_txt更新

--- image_to_txt.py
+++ image_to_txt.py
@@ -1,13 +1,17 @@
 from PIL.Image import open
+from tkinter import Tk, filedialog
 from sys import exit
 from pyperclip import copy
 from pyocr import tesseract, get_available_tools
 from pyocr.builders import TextBuilder
 
-OS = 'Lin'
+OS = 'Win'
 LANG = 'jpn'
 
 def image_to_txt(os, lang):
+    root = Tk()
+    root.lower()
+    root.withdraw()
     if os  == 'Win':
         tesseract.TESSERACT_CMD = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
@@ -15,20 +19,22 @@ def image_to_txt(os, lang):
     if len(tools) == 0:
         print("No OCR tool found")
         exit(1)
-    # The tools are returned in the recommended order of usage
     tool = tools[0]
     print("Will use tool '%s'" % (tool.get_name()))
-    # Ex: Will use tool 'libtesseract'
 
     langs = tool.get_available_languages()
     print("Available languages: %s" % ", ".join(langs))
     print("Will use lang '%s'" % (lang))
-    # Ex: Will use lang 'fra'
-    # Note that languages are NOT sorted in any way. Please refer
-    # to the system locale settings for the default language
-    # to use.
 
-    txt = tool.image_to_string(open('test.png'), lang=lang, builder=TextBuilder())
+    f = filedialog.askopenfilename()
+    root.deiconify()
+    root.withdraw()
+    if not f:
+        print('canceled.')
+        exit(1)
+    print(f)
+
+    txt = tool.image_to_string(open(f), lang=lang, builder=TextBuilder())
     # txt is a Python string
     print(f'{"-"*50}\n{txt}\n{"-"*50}')
     copy(txt)
