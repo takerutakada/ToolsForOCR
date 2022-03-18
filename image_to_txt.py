aa00817c2f0c4333b7c7e0893737e69d08d53663
takerutakada <69188579+takerutakada@users.noreply.github.com>
2022-03-18 16:05:30 +0900

    GUIで言語選択可能化

--- image_to_txt.py
+++ image_to_txt.py
@@ -1,5 +1,6 @@
 from PIL.Image import open
-from tkinter import Tk, filedialog
+from tkinter import Tk, simpledialog, filedialog
+from tkinter.messagebox import showinfo
 from sys import exit
 from pyperclip import copy
 from pyocr import tesseract, get_available_tools
@@ -7,9 +8,8 @@ from pyocr.builders import TextBuilder
 
 # Run with Windows→'Win'/Linux→'Lin'
 OS = 'Win'
-LANG = 'jpn'
 
-def image_to_txt(os, lang):
+def image_to_txt(os):
     '''
     Converting strings in an image to text
     '''
@@ -28,9 +28,10 @@ def image_to_txt(os, lang):
     tool = tools[0]
     print("Will use tool '%s'" % (tool.get_name()))
 
+    langs_idx = simpledialog.askinteger('言語選択','英語→0\n日本語（横書き）→1\n日本語（縦書き）→2')
     langs = tool.get_available_languages()
     print("Available languages: %s" % ", ".join(langs))
-    print("Will use lang '%s'" % (lang))
+    print("Will use lang '%s'" % (langs[int(langs_idx)]))
 
     f = filedialog.askopenfilename()
     root.deiconify()
@@ -40,10 +41,11 @@ def image_to_txt(os, lang):
         exit(1)
     print(f)
 
-    txt = tool.image_to_string(open(f), lang=lang, builder=TextBuilder())
-    print(f'{"-"*50}\n{txt}\n{"-"*50}')
+    txt = tool.image_to_string(open(f), lang=langs[int(langs_idx)], builder=TextBuilder())
     copy(txt)
+    print(f'{"-"*50}\n{txt}\n{"-"*50}')
     print('copied to clipboard.')
+    showinfo('テキスト抽出結果', f'以下のテキストをクリップボードにコピーしました：\n{txt}')
 
 if __name__ == '__main__':
-    image_to_txt(OS,LANG)
+    image_to_txt(OS)
